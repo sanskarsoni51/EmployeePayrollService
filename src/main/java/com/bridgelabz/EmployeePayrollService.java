833316029f504af192202f3df502b877c2be2683
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-19 12:20:26 +0530

    added: functionality to read and write Employee payroll to console

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -5,7 +5,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+    static List<Employee> EmployeeList = new ArrayList<>();
+
+    public static void ReadEmployeeData(Scanner sc){
+        System.out.println("Enter Employee id");
+        int id = sc.nextInt();
+        System.out.println("Enter Employee Name");
+        String name = sc.next();
+        System.out.println("Enter Employee salary");
+        int salary = sc.nextInt();
+        EmployeeList.add(new Employee(id,name,salary));
+    }
+
+    public static void WriteEmployeeData(){
+        System.out.println("Writing Employee payroll to console" + EmployeeList);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Payroll Service");
+        Scanner sc = new Scanner(System.in);
+        EmployeePayrollService.ReadEmployeeData(sc);
+        EmployeePayrollService.WriteEmployeeData();
     }
 }
